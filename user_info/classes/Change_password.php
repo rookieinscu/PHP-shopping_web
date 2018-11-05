a3f4f84323adcfbe6586303371e4bf9b4499391b
rookieinscu <43287635+rookieinscu@users.noreply.github.com>
2018-11-05 14:51:46 +0800

    Add files via upload

--- user_info/classes/Change_password.php
+++ user_info/classes/Change_password.php
@@ -1,7 +1,8 @@
 <?php 
-class Change_password{
-	private function change_user_password(){
-		session_start();
+$changepassword = new Changepassword();
+class Changepassword{
+	public function __construct(){
+		session_start(); 
 		require_once("../../dbconnection/db.php");
 			if ($_POST['user_password_new'] !== $_POST['user_password_repeat']) {
 			echo "<script>alert('两次输入的新密码不一致')</script>";
@@ -18,7 +19,6 @@ class Change_password{
 				         FROM user_info
 						 WHERE user_id = '" .$user_id. "';";
 				$query_check_password = $this->db_connection->query($sql1);
-				$old_password = $_POST['user_password'];
 				if($_POST['user_password'] == $result_row->user_password){				
 					$sql2 = "UPDATE user_info 
 						SET user_password=" . $_POST['user_password_new'] . "
@@ -27,8 +27,9 @@ class Change_password{
                 	if ($query_new_user_changepass) {
 						echo "<script>alert('密码修改成功，请您重新登录');
 						window.location.href='../../login/index_yes.php'</script>";
-					} else {echo "<script>alert('密码修改失败！')</script>";}
+					} else {echo "<script>alert('密码修改失败，请重试或请联系管理员')</script>";}
 			}else {echo "<script>alert('原密码不正确！')</script>";}
 			}
 	}
 }
+?>
