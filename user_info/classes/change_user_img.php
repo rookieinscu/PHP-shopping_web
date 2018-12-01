dc4484d1dbb5b1f7e66f86fc9d6b1cb915b7057d
rookieinscu <43287635+rookieinscu@users.noreply.github.com>
2018-12-01 19:14:57 +0800

    Delete change_user_img.php

--- user_info/classes/change_user_img.php
+++
@@ -1,6 +0,0 @@
-<p>hello<?php session_start(); echo $_SESSION['user_nickname']; ?> ，您正在修改头像<p>
-<form action="../classes/Change_user_img.php" method="post" enctype="multipart/form-data">
-请选择您要上传的文件：<input type="file" name='user_img' />
-<input type="submit" value="上传文件" />
-</form>
-<a href="show_user_info.php">放弃</a>
